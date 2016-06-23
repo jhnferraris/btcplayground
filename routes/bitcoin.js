8b18cfc37c679255698dede077ba219864ca9e32
John Ferrraris <johnmichaelferraris@gmail.com>
2016-06-23 14:33:16 +0800

    Added 5-day Simple Moving average

--- routes/bitcoin.js
+++ routes/bitcoin.js
@@ -4,6 +4,60 @@ var _ = require('underscore');
 
 var router = express.Router();
 
+function formatHistoricalData(historicalData) {
+  var formattedHistoricalData = _.map(historicalData, function (value, key) {
+    return {
+      'date': key,
+      'price': value
+    }
+  });
+
+  // This is a 5 day MA
+  var movingAverages = simpleMovingAverage(formattedHistoricalData);
+  // console.log(movingAverages);
+  var ret = [];
+  var index = 0;
+  formattedHistoricalData.forEach( function (data, key) {
+    var item = null;
+    if (key >= 4) {
+      item = {
+        'date': data.date,
+        'price': data.price,
+        'sma': movingAverages[index]
+      };
+      index++;
+    } else {
+      item = {
+        'date': data.date,
+        'price': data.price,
+      };
+    }
+
+    ret.push(item);
+  });
+  return ret;
+}
+
+// Source: http://jsfiddle.net/plmrry/ktLtN/
+function simpleMovingAverage(data) {
+  var priceData = _.pluck(data, 'price');
+  var numberOfPeriods = 5;
+  var movingAverage = priceData.map(function(each, index, array) {;
+    var to = index + numberOfPeriods - 1;
+    var subSeq, sum;
+    if (to < priceData.length) {
+      subSeq = array.slice(index, to + 1);
+      console.log(subSeq);
+      sum =  _.reduce(subSeq, function(memo, num){ return memo + num; }, 0);
+      console.log(sum);
+      return (sum / numberOfPeriods);
+      }
+      return undefined;
+    });
+
+  return movingAverage
+}
+
 router.get('/bitcoin', function(req, res, next) {
   http.get({
     host: 'api.coindesk.com',
@@ -16,13 +70,7 @@ router.get('/bitcoin', function(req, res, next) {
       response.on('end', function() {
         // Data reception is done, do whatever with it!
         var parsed = JSON.parse(body);
-        var historicalData = _.map(parsed.bpi, function (value, key) {
-          return {
-            'date': key,
-            'price': value
-          }
-        });
-        historicalData.reverse()
+        var historicalData = formatHistoricalData(parsed.bpi);
         // res.send(historicalData);
         res.render('history', {historicalData});
         });
